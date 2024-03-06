56f771bef4c8ae983f8f0661868c6c508088c0a0
william <williamkotronakis@gmail.com>
2024-03-06 17:06:54 +0200

    feat: initial lang select dropdown

---
+++ src/components/Language/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { localeCodes, localeOptions, localeResources } from '@constants/locales';
+import classNames from 'classnames';
+import "./index.css";
+
+const isLangValid = (lang: string) => localeCodes.includes(lang);
+
+const LanguageSwitch: React.FC = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (isLangValid(e.target.value)) i18n.changeLanguage(e.target.value);
+  }
+
+  return (
+    <select onChange={changeLanguage} className="language__switch" value={i18n.language}>
+      {localeCodes.map((code) => (
+        <option
+          className={classNames("language__option", { "active": code === i18n.language })}
+          key={code}
+          value={code}
+        >
+          {localeOptions[code].native}
+        </option>
+      ))}
+    </select>
+  )
+}
+export default LanguageSwitch
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,14 @@
+import LanguageSwitch from '@components/Language';
 import React from 'react';
-
 
 const Navbar: React.FC = () => {
   return (
-    <header className="bg-red-900 py-2 px-4">Elgainulator 3000</header>
+    <header className="bg-red-900 py-2 px-4 flex">
+      <h1>Elgainulator 3000</h1>
+      <div className="ml-auto">
+        <LanguageSwitch />
+      </div>
+    </header>
   )
 }
 export default Navbar;
