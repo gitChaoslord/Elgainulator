1c852d52e775a8984db0b987241a4a8aa3c5ce33
william <williamkotronakis@gmail.com>
2024-03-15 20:09:19 +0200

    feat: added constants file for calc variables

---
+++ src/constants/calculator.ts
@@ -0,0 +1,6 @@
+export const THEME_OPTIONS = [
+  { label: "THEMES.DEFAULT", value: "default", },
+  { label: "THEMES.SPACE", value: "space", className: "bg-gray-500 text-white" }
+];
+
+export const MAX_NAME_LIMIT = 30;
